38737bb3db2de6e94ecb8867a1296e3112be1b5b
S.chandra thakur <subhashk012005@gmail.com>
2026-02-18 06:56:32 +0530

    Add files via upload
    loops in python

---
+++ loops.py
@@ -0,0 +1,16 @@
+#print star pattern using loops
+n=5 
+for i in range(1,n+1):
+     print("*"*i)
+
+#inverted stR pattern
+#n=5 
+#for i in range(n,0,-1):
+  #  print("*"*i)
+
+    #pyramid pattern using loop
+    
+#n=5
+#for i in range(1,n+1):
+        #print(' '*(n-i),end=" ")
+        #print("*"*(2*i-1))
