608eae79ab8436abff3c5355a82af4aeade1a66d
S.chandra thakur <subhashk012005@gmail.com>
2026-02-18 06:58:00 +0530

    loops.py

--- loops.py
+++ loops.py
@@ -4,13 +4,14 @@ for i in range(1,n+1):
      print("*"*i)
 
 #inverted stR pattern
-#n=5 
-#for i in range(n,0,-1):
-  #  print("*"*i)
+n=5 
+for i in range(n,0,-1):
+    print("*"*i)
 
     #pyramid pattern using loop
     
-#n=5
-#for i in range(1,n+1):
-        #print(' '*(n-i),end=" ")
-        #print("*"*(2*i-1))
+n=5
+for i in range(1,n+1):
+     print(' '*(n-i),end=" ")
+
+        print("*"*(2*i-1))
